66f6a1208968853576847e76e0c7638efd99d980
葛胤池 <geyinchi@buaa.edu.cn>
2022-12-12 17:42:12 +0800

    fix: forgot to upload config

--- config/index.js
+++ config/index.js
@@ -6,5 +6,9 @@ export const Config = {
   cfClearance: CF_CLEARANCE,
   proxy: PROXY,
   username: '',
-  password: ''
+  password: '',
+  // 改为true后，全局默认以图片形式回复，并自动发出Continue命令补全回答
+  defaultUsePicture: false,
+  // 每个人发起的对话保留时长。超过这个时长没有进行对话，再进行对话将开启新的对话。单位：秒
+  conversationPreserveTime: 600,
 }
